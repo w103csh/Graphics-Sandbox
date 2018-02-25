9b4495c4f91e41a97cfdc0d13755cd35b1524b46
w103csh <cloiniffty@gmail.com>
2018-02-25 12:41:38 -0700

    pdfTest to textures in scene

--- client/scripts/graphicsBook1.js
+++ client/scripts/graphicsBook1.js
@@ -1,8 +1,10 @@
 // globals
-var camera, scene, renderer;
+var camera, scene, renderer, canvas;
+var pdfTest = new PDFTest('pdfTest', 'header', createPdfTextureGraphicsBook1, 'width');
+var maxDistance = 500000;
 
 // test vars for animation
-var animate = false;
+var doAnimate = true;
 var start = null;
 var last = null;
 var animationLength = 5;
@@ -10,7 +12,7 @@ var animationRotation = null;
 var testAngle = null;
 
 init();
-animate && rotateScene();
+doAnimate && animate();
 
 function init() {
   let canvasParent = document.getElementById('graphicsBook1');
@@ -24,18 +26,18 @@ function init() {
   );
   // X: RED || Y: GREEN || Z: BLUE
   // console.log(camera);
-  camera.position.set(60, 30, 50);
+  camera.position.set(50, 30, 50);
   camera.lookAt(0, 0, 0);
 
   scene = new THREE.Scene();
-
-  // Hello World! mesh
-  // scene.add(getHelloWorldMesh());
 
   let axes = getAxisLinesGeom();
   axes.forEach(axis => {
     scene.add(axis);
   });
+
+  let workPlane = getWorkPlane();
+  scene.add(workPlane);
   
   // let a = getRandomLineGeom();
   // scene.add(a);
@@ -49,24 +51,16 @@ function init() {
   renderer.setPixelRatio(canvasParent.devicePixelRatio);
   renderer.setSize(canvasParent.clientWidth, canvasParent.clientHeight);
   renderer.setClearColor(0x000000, 1);
-  !animate && renderer.render(scene, camera);
+  // Single render if not ticking
+  !doAnimate && renderer.render(scene, camera);
 
-  canvasParent.appendChild(renderer.domElement);
-}
-
-function getHelloWorldMesh() {
-  let geometry = new THREE.IcosahedronGeometry(200, 1);
-  let material = new THREE.MeshBasicMaterial({
-    color: 0xfff999fff,
-    wireframe: true,
-    wireframeLinewidth: 8
-  });
-  return new THREE.Mesh(geometry, material);
+  canvas = renderer.domElement;
+  canvasParent.appendChild(canvas);
 }
 
 function getAxisVector(axis, direction) {
   //console.log(axis);
-  max = 500000*direction;
+  max = maxDistance * direction;
   let x = axis === "X" ? max : 0;
   let y = axis === "Y" ? max : 0;
   let z = axis === "Z" ? max : 0;
@@ -128,28 +122,108 @@ function makeGeom() {
   return geometry;
 }
 
-var notifyLength = true;
-var rotSoFar = 0;
-function rotateScene() {
-  window.requestAnimationFrame(rotateScene);
+function getWorkPlane() {
+  let scale = 1;
 
-  let now = Date.now() / 1000 ; // now in seconds
-  start = start || now;
-  let elapsed = now - start;
+  let geometry = new THREE.PlaneGeometry(
+    maxDistance * scale, // width
+    maxDistance * scale, // height
+    16 // width segments
+    // height segements
+  );
 
-  if( elapsed <= animationLength ) {
-    let rotPercent = animationRotation * ( elapsed / animationLength );
-    let rot = rotPercent - rotSoFar; // only rotate the the amount percent since calc.
-    rotSoFar = rotPercent;
-    console.log(rot);
-    camera.rotateZ( rot );
-    camera.updateProjectionMatrix();
-  }
-  else if (notifyLength) {
-    notifyLength = false;
-    console.log(`elapsed: ${elapsed}`);
-    debugger;
-  }
+  let material = new THREE.MeshBasicMaterial({
+    color: 0xb0b0b0,
+    side: THREE.DoubleSide,
+    transparent: true,
+    opacity: 0.5
+  });
 
+  let plane = new THREE.Mesh( geometry, material );
+  plane.name = 'workPlane';
+  plane.rotateX( Math.PI / 2 );
+
+  return plane;
+}
+
+function createPDFRect(material) {
+  // // Get image dimension info
+  // let scale = 1;
+  // let w_half = ( material.map.image.width  / 2 ) * scale;
+  // let h_half = ( material.map.image.height / 2 ) * scale;
+
+  // let rectGeom = new THREE.Geometry();
+  // // These are y up hardcoded for now
+  // rectGeom.vertices.push(new THREE.Vector3( - w_half, 0.0, - h_half )); 
+  // rectGeom.vertices.push(new THREE.Vector3(   w_half, 0.0,   h_half )); 
+  // rectGeom.vertices.push(new THREE.Vector3( - w_half, 0.0,   h_half )); 
+  // rectGeom.vertices.push(new THREE.Vector3(   w_half, 0.0, - h_half )); 
+  // rectGeom.faces.push(new THREE.Face3(0, 1, 2));
+  // rectGeom.faces.push(new THREE.Face3(0, 2, 3));
+
+  // Just make a box for now.
+  let baseSize = 50;
+  let relWidth = ( material.map.image.width / material.map.image.height ) * baseSize;
+  let relHeight = ( material.map.image.height / material.map.image.width ) * baseSize;
+  let geometry = new THREE.BoxBufferGeometry( relWidth, 2, relHeight );
+
+  let rectMesh = new THREE.Mesh(geometry, material);
+  rectMesh.name = 'PDFRect';
+
+  scene.add(rectMesh); 
+}
+
+function createPdfTextureGraphicsBook1(texture) {
+  console.log('PDF is now a texture. WTF! GB');
+
+  // Test image quality
+  let div = document.createElement('div');
+  let header = document.createElement('h3');
+  header.textContent = 'Image quality test';
+  div.appendChild(header);
+  let img = document.createElement('img');
+  img.src = texture.image.currentSrc;
+  div.appendChild(img);
+  document.body.appendChild(div);
+  
+  let material = new THREE.MeshBasicMaterial({
+    map: texture,
+  });
+  createPDFRect(material);
+}
+
+function animate() {
+  window.requestAnimationFrame(animate);
+
+  // rotateScene();
+  
   renderer.render(scene, camera);
+}
+
+// Animation functions
+
+// This is boned atm.
+var rotateScene = function() {
+  var notifyLength = true;
+  var rotSoFar = 0;
+
+  return function() {
+    let now = Date.now() / 1000 ; // now in seconds
+    start = start || now;
+    let elapsed = now - start;
+  
+    if( elapsed <= animationLength ) {
+      let rotPercent = animationRotation * ( elapsed / animationLength );
+      let rot = rotPercent - rotSoFar; // only rotate the the amount percent since calc.
+      rotSoFar = rotPercent;
+      console.log(rot);
+      camera.rotateZ( rot );
+      camera.updateProjectionMatrix();
+    }
+    else if (notifyLength) {
+      notifyLength = false;
+      console.log(`elapsed: ${elapsed}`);
+      debugger;
+    }
+  }
 }
--- client/scripts/pdfTest.js
+++ client/scripts/pdfTest.js
@@ -1,92 +1,141 @@
+var PDFTest = (function PDFTestClosure() {
 
-const min_pdfCanvasWidth = 700;
-const min_pdfCanvasHeight = 550;
+  var __instance = null;
+  var __callback = null;
+  var __fileReader = new FileReader();
 
-// the this object is boned
+  class PDFTest {
+    constructor(parentId, inputId, callback, parentScaleTo) {
+      // Input
+      let header = document.getElementById(inputId);
+      // Parent
+      let parent = document.getElementById(parentId);
+      // Set scaleTo (default to width)
+      this._parentScaleTo = parentScaleTo === 'height' ? parentScaleTo : 'width';
 
-class pdfTest {
-  constructor() {
-    // Header
-    let header = document.getElementById('header');
-    // File input
-    let fileInput = document.createElement('input');
-    fileInput.type = 'file';
-    fileInput.onchange = this.handleFiles;
-    header.appendChild(fileInput);
-  
-    // pdfTest
-    let pdfTest = document.getElementById('pdfTest');
-    // Canvas
-    this.canvas = document.createElement('canvas');
-    this.canvas.style.width = '100%';
-    this.canvas.style.height = '100%';
-    pdfTest.appendChild(this.canvas);
+      // File input
+      let fileInput = document.createElement('input');
+      fileInput.type = 'file';
+      fileInput.onchange = handleFiles;
+      header.appendChild(fileInput);
+    
+      // Canvas
+      this._canvas = document.createElement('canvas');
+      // Fill parent by default
+      this._canvas.style.width = '100%';
+      this._canvas.style.height = '100%';
+      parent.appendChild(this._canvas);
 
-    this.fileReader = new FileReader();
-    this.fileReader.onload = this.pdfLoaded;
-  }
+      __fileReader.onload = pdfLoaded;
 
-  handleFiles() {
-    for (let i = 0; i < this.files.length; i++) {
-      let file = this.files.item(i);
-      _pdfTest.fileReader.readAsArrayBuffer(file);
+      // Expose pdf rendered so that it can be overridden.
+      // TODO: change callback thing
+      this.pageLoaded = pageLoaded;
+
+      // Set __instance to this. I couldn't think of a better way but I am sure there is one.
+      __instance = this;
+      __callback = callback;
     }
   }
 
-  pdfLoaded() {
+  // Private functions
+
+  function pdfLoaded() {
     let array = new Uint8Array(this.result);
     let loadingTask = PDFJS.getDocument(array);
     loadingTask.promise.then((pdf) => {
       console.log('PDF loaded');
 
+      // TODO: loop through pages
       if (!pdf.numPages) {
         console.log('No pages');
         return;
       }
-      pdf.getPage(1).then(_pdfTest.pageLoaded);
+      pdf.getPage(1).then(pageLoaded);
 
     });
   }
 
-  pageLoaded(page) {
+  function handleFiles() {
+    for (let i = 0; i < this.files.length; i++) {
+      let file = this.files.item(i);
+      __fileReader.readAsArrayBuffer(file);
+    }
+  }
+
+  function pageLoaded(page) {
     console.log('Page loaded');
 
-    // Make sure _pdfTest.canvas width and height are updated.
-    _pdfTest.canvas.width = Math.max(_pdfTest.canvas.clientWidth, min_pdfCanvasWidth);
-    _pdfTest.canvas.height = Math.max(_pdfTest.canvas.clientHeight, min_pdfCanvasHeight);
+    // Make sure canvas width and height are updated if percent css used.
+    __instance._canvas.width = __instance._canvas.clientWidth;
+    __instance._canvas.height = __instance._canvas.clientHeight;
 
+    // Get file's info
     let scale = 1;
     let viewport = page.getViewport(scale);
 
     let setScale = true;
 
     if (setScale) {
-      // Find scale for _pdfTest.canvas size
-      let w_prop = _pdfTest.canvas.width / viewport.width;
-      let h_prop = _pdfTest.canvas.height / viewport.height;
-      scale = Math.min(w_prop, h_prop);
+      // TODO: save original viewport information for the texture before
+      // changing for scaled render below
 
-      let rect = [0, 0, _pdfTest.canvas.width, _pdfTest.canvas.height];
+      // Find scale for canvas
+      let scaleToHeight = __instance._parentScaleTo === 'height';
+      let dimensions = getDimensions(viewport, scaleToHeight);
+
+      // Set new canvas dimension
+
+      // This makes the image square to prevent distortion (doesn't work)
+      // __instance._canvas.width = scaleToHeight ? dimensions.height : dimensions.width;
+      // __instance._canvas.height = scaleToHeight ? dimensions.height : dimensions.width;
+
+      // This makes it look nice
+      __instance._canvas.height = dimensions.width;
+      __instance._canvas.height = dimensions.height;
+
+      let rect = [
+        0,
+        0,
+        __instance._canvas.width,
+        __instance._canvas.height
+      ];
 
       // Set viewport to the largest size where the whole pdf is visible.
-      viewport = page.getViewport(scale);
+      viewport = page.getViewport(dimensions.scale);
       viewport.convertToViewportRectangle(rect);
     }
 
     var renderTask = page.render({
-      canvasContext: _pdfTest.canvas.getContext('2d'),
+      canvasContext: __instance._canvas.getContext('2d'),
       viewport: viewport
     });
 
-    renderTask.then(_pdfTest.pageRendered);
+    renderTask.then(pageRendered);
   }
 
-  pageRendered() {
+  function pageRendered() {
     console.log('Page rendered.');
-    let image = _pdfTest.canvas.toDataURL();
-    _textureLoader.load(image, createPdfTexture);
+    let image = __instance._canvas.toDataURL();
+    _textureLoader.load(image, __callback);
   }
 
-}
+  function getDimensions(viewport, scaleToHeight) {
+    let width, height, scale;
 
-var _pdfTest = new pdfTest();
+    scale = scaleToHeight ?
+      ( __instance._canvas.height / viewport.height ) :
+      ( __instance._canvas.width / viewport.width );
+
+    width = scale * viewport.width;
+    height = scale * viewport.height;
+
+    return { 
+      scale: scale,
+      width: width,
+      height: height,
+    };
+  }
+
+  return PDFTest;
+})();
