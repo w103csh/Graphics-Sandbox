41b30c57f1a2379d29060b1fa458ff90b6a6f6ba
w103csh <cloiniffty@gmail.com>
2018-03-05 18:15:12 -0700

    testing some things

--- client/scripts/pdfTest.js
+++ client/scripts/pdfTest.js
@@ -3,6 +3,7 @@ var PDFTest = (function PDFTestClosure() {
   var __instance = null;
   var __callback = null;
   var __fileReader = new FileReader();
+  var DEFAULT_SCALE = 1.5;
 
   class PDFTest {
     constructor(parentId, inputId, callback, parentScaleTo) {
@@ -35,26 +36,39 @@ var PDFTest = (function PDFTestClosure() {
       // Set __instance to this. I couldn't think of a better way but I am sure there is one.
       __instance = this;
       __callback = callback;
+
+      // Promise.all([System.import('pdfjs/display/api'),
+      //   System.import('pdfjs/display/svg'),
+      //   System.import('pdfjs/display/global'),
+      //   System.import('pdfjs/display/worker_options'),
+      //   System.import('pdfjs/display/network'),
+      //   System.resolve('pdfjs/worker_loader')])
+      // .then(function (modules) {
+      //   var api = modules[0];
+      //   var svg = modules[1];
+      //   var global = modules[2];
+      //   var GlobalWorkerOptions = modules[3].GlobalWorkerOptions;
+      //   var network = modules[4];
+      //   api.setPDFNetworkStreamFactory((params) => {
+      //     return new network.PDFNetworkStream(params);
+      //   });
+
+      //   // In production, change this to point to the built `pdf.worker.js` file.
+      //   GlobalWorkerOptions.workerSrc = modules[5];
+
+      //   // In production, change this to point to where the cMaps are placed.
+      //   global.PDFJS.cMapUrl = '../../external/bcmaps/';
+      //   global.PDFJS.cMapPacked = true;
+
+      //   // Fetch the PDF document from the URL using promises.
+      //   api.getDocument(url).then(function (doc) {
+      //     renderDocument(doc, svg);
+      //   });
+      // });
     }
   }
 
   // Private functions
-
-  function pdfLoaded() {
-    let array = new Uint8Array(this.result);
-    let loadingTask = PDFJS.getDocument(array);
-    loadingTask.promise.then((pdf) => {
-      console.log('PDF loaded');
-
-      // TODO: loop through pages
-      if (!pdf.numPages) {
-        console.log('No pages');
-        return;
-      }
-      pdf.getPage(1).then(pageLoaded);
-
-    });
-  }
 
   function handleFiles() {
     for (let i = 0; i < this.files.length; i++) {
@@ -63,8 +77,24 @@ var PDFTest = (function PDFTestClosure() {
     }
   }
 
+  function pdfLoaded() {
+    let array = new Uint8Array(this.result);
+    PDFJS.getDocument(array).then((doc) => {
+      console.log('PDF loaded');
+
+      // TODO: loop through pages
+      if (!doc.numPages) {
+        console.log('No pages');
+        return;
+      }
+      doc.getPage(1).then(pageLoaded);
+    });
+  }
+
   function pageLoaded(page) {
     console.log('Page loaded');
+
+    page.getOperatorList().then(parseOpList);
 
     // Make sure canvas width and height are updated if percent css used.
     __instance._canvas.width = __instance._canvas.clientWidth;
@@ -114,6 +144,21 @@ var PDFTest = (function PDFTestClosure() {
     renderTask.then(pageRendered);
   }
 
+  function parseOpList(opList) {
+    console.log(opList);
+    let ops = new Set(opList.fnArray);
+    let opsNames = [];
+    ops.forEach((op) => {
+      opsNames.push(getKeyByValue(PDFJS.OPS, op));
+    });
+    // opList.fnArray.forEach((fn, index) => {
+    //   if(fn == PDFJS.OPS.lineTo) {
+    //     console.log('tear');
+    //     lines.push(opList.argsArray[index]);
+    //   }
+    // });
+  }
+
   function pageRendered() {
     console.log('Page rendered.');
     __callback(__instance._canvas);
@@ -134,6 +179,10 @@ var PDFTest = (function PDFTestClosure() {
       width: width,
       height: height,
     };
+  }
+
+  function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
   }
 
   return PDFTest;
